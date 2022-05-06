f692184d20bc7c10bfa449f94ca0fa776c4e2194
jesus1811 <jayarza1811@gmail.com>
2022-05-06 09:50:09 -0500

    update images services

--- pages/services/components/servicio/Servicio.jsx
+++ pages/services/components/servicio/Servicio.jsx
@@ -5,7 +5,7 @@ const Servicio = ({ servicio }) => {
   return (
     <article className={styles.card}>
       <h1 className={styles.title}>{servicio.NombreServicio}</h1>
-      <img src="/time.jpg" alt="" className={styles.images} />
+      <img src={servicio.foto} alt="" className={styles.images} />
       <p className={styles.precio}>S/. {servicio.precio}</p>
       <p className={styles.profesional}>{servicio.nombreProfesional + " " + servicio.apellidoProfesional}</p>
       <div className={styles.containerButtons}>
