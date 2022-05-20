148497f4c7ebbc9af29bb9ef46ff12a3fcaf38c3
jesus1811 <jayarza1811@gmail.com>
2022-05-19 20:47:08 -0500

    add age404

--- pages/404.page.jsx
+++ pages/404.page.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import { useContext } from "react";
 import { ContainerPrimary, Header } from "../components/layouts";
+import { DataContext } from "../context/Provider";
+import styles from "./page404.module.scss";
 
-const Custom404= () => {
+const Custom404 = () => {
+  const { store } = useContext(DataContext);
   return (
     <>
       <ContainerPrimary>
         <Header />
-        <h1>hola mundo soy el 404</h1>
+        <img src="/error404.svg" alt="" />
+        <h1 className={store.onDark ? styles.titleDark : styles.title}>Pagina no Encontrada</h1>
       </ContainerPrimary>
     </>
   );
