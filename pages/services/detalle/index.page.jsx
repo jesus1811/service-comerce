1cad76eee1562ef068c186dc7ccb54d5dc61cc95
jesus1811 <jayarza1811@gmail.com>
2022-06-02 13:52:07 -0500

    add valoracion get valoracion

--- pages/services/detalle/index.page.jsx
+++ pages/services/detalle/index.page.jsx
@@ -1,23 +1,32 @@
 import { useEffect, useState, useContext } from "react";
-import { Button, Loading } from "../../../components/common";
+import { Button, Input, Loading } from "../../../components/common";
 import { ContainerPrimary, Header } from "../../../components/layouts";
 import { DataContext } from "../../../context/Provider";
+import useField from "../../../hooks/useField";
 import { postComprobanteServices } from "../../../services/comprobante.service";
 import { getProfesionalServices } from "../../../services/profesional.service";
 import { getServicioIdServices } from "../../../services/servicio.service";
+import { getValoraciones, postValoracion } from "../../../services/valoracion.service";
 import styles from "./detalle.module.scss";
 
 const Detalle = () => {
   const [servicio, setServicio] = useState([]);
   const [profesional, setProfesional] = useState([]);
   const [loader, setLoader] = useState(true);
-  const [total, setTotal] = useState(null);
+  const [loaderProfesional, setLoaderProfesional] = useState(true);
+  const [loaderValoraciones, setLoaderValoraciones] = useState(true);
+  const [valoraciones, setValoraciones] = useState([]);
+  const comentario = useField("text");
   const { store } = useContext(DataContext);
   useEffect(() => {
     const { idServicio, idProfesional } = JSON.parse(localStorage.getItem("servicio"));
     getServicioIdServices(idServicio, setServicio, setLoader);
-    getProfesionalServices(idProfesional, setProfesional, setLoader);
-  }, [loader]);
+    getProfesionalServices(idProfesional, setProfesional, setLoaderProfesional);
+  }, []);
+  useEffect(() => {
+    const { idServicio } = JSON.parse(localStorage.getItem("servicio"));
+    getValoraciones(idServicio, setValoraciones, setLoaderValoraciones);
+  }, [loaderValoraciones]);
   const fecha = new Date().getDate() + "/" + (new Date().getMonth() + 1) + "/" + new Date().getFullYear();
   const handleClickPlin = () => {
     postComprobanteServices(
@@ -31,15 +40,19 @@ const Detalle = () => {
   const handleClickYape = () => {
     postComprobanteServices(fecha, store.user[0].idCliente, 1, JSON.parse(localStorage.getItem("servicio")).idServicio);
   };
+  const handleClickAddValoracion = () => {
+    const { idServicio } = JSON.parse(localStorage.getItem("servicio"));
+    postValoracion(comentario.value, idServicio, store?.user[0]?.idCliente, setLoaderValoraciones);
+  };
 
   return (
     <ContainerPrimary>
       <Header />
+      <h1 className={store.onDark ? styles.titleDark : styles.title}>Detalle del Servicio</h1>
       {loader ? (
         <Loading />
       ) : (
         <>
-          <h1 className={store.onDark ? styles.titleDark : styles.title}>Detalle del Servicio</h1>
           <article className={store.onDark ? styles.cardDark : styles.card}>
             <p className={styles.nameService}>
               {servicio[0]?.descuento > 0 && "en Oferta " + servicio[0]?.descuento + "%"}
@@ -63,7 +76,40 @@ const Detalle = () => {
             <Button onClick={handleClickPlin}>pagar por Plin</Button>
             <Button onClick={handleClickYape}>pagar por Yape</Button>
           </article>
-          <h1 className={store.onDark ? styles.titleDark : styles.title}>Informacion Personal del Provedor</h1>
+        </>
+      )}
+      <h1 className={store.onDark ? styles.titleDark : styles.title}>Comentarios</h1>
+      {loaderValoraciones ? (
+        <Loading />
+      ) : (
+        <>
+          {valoraciones.map((valoracion, index) => {
+            return (
+              <>
+                <article className={store.onDark ? styles.cardClienteDark : styles.cardCliente} key={index}>
+                  <div className={styles.user}>
+                    <img className={styles.imageCliente} src={valoracion.urlFoto} alt="" />
+                    <p className={styles.nameCenter}>
+                      {valoracion.nombreCliente} {valoracion.apellidoCliente}
+                    </p>
+                  </div>
+                  <p className={styles.nameCenter}>{valoracion.comentario}</p>
+                  {/* <p className={styles.nameCenter}>{JSON.stringify(valoracion)}</p> */}
+                </article>
+              </>
+            );
+          })}
+          <article className={store.onDark ? styles.cardDarkComentario : styles.cardComentario}>
+            <Input {...comentario} />
+            <Button onClick={handleClickAddValoracion}>Agregar Comentario</Button>
+          </article>
+        </>
+      )}
+      <h1 className={store.onDark ? styles.titleDark : styles.title}>Informacion Personal del Provedor</h1>
+      {loaderProfesional ? (
+        <Loading />
+      ) : (
+        <>
           <article className={store.onDark ? styles.cardDark : styles.card}>
             <img className={styles.image} src={profesional[0]?.urlFoto} alt="" />
             <h1 className={styles.nameService}>
---
+++ services/valoracion.service.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import Swal from "sweetalert2";
+const BASE_URL = process.env.NEXT_PUBLIC_URL;
+
+export const getValoraciones = async (idServicio, setData, setLoader) => {
+  await axios
+    .get(BASE_URL + "/api/valoraciones/" + idServicio)
+    .then(({ data }) => {
+      setData(data);
+      setLoader(false);
+    })
+    .catch((err) => {
+      console.log(err);
+      Swal.fire({
+        title: "error de conexion con el servidor",
+        icon: "error",
+      });
+    });
+};
+export const postValoracion = async (comentario, idServicio, idCliente, setLoader) => {
+  await axios
+    .post(BASE_URL + "/api/valoracion", {
+      comentario,
+      idServicio,
+      idCliente,
+    })
+    .then(({ data }) => {
+      Swal.fire({
+        title: data,
+        icon: "success",
+      });
+      setLoader(true);
+    })
+    .catch((err) => {
+      console.log(err);
+      Swal.fire({
+        title: "error de conexion con el servidor",
+        icon: "error",
+      });
+    });
+};
