208cdea7cc7211f5e2c49d526ec315fda76e462f
jesus1811 <jayarza1811@gmail.com>
2022-06-07 15:00:44 -0500

    update bug

--- pages/index.page.jsx
+++ pages/index.page.jsx
@@ -5,7 +5,8 @@ import { DataContext } from "../context/Provider";
 import useField from "../hooks/useField";
 import { validarclienteServices } from "../services/cliente.service";
 import { Button, Description, Input, Subtitle, Title, Card } from "../styled-components";
-import { ContainerButtons, ContainerInputs, Error, Image } from "./Styled";
+import {  ContainerInputs, Error, Image } from "./Styled";
+import { ContainerButtons } from "./styled-components";
 
 const Home = () => {
   const email = useField("email");
@@ -34,7 +35,7 @@ const Home = () => {
   return (
     <Container>
       <Card center>
-        <Title center>MONTALVO edit ahora si</Title>
+        <Title center>MONTALVO</Title>
         <Description>plataforma de multiservicios</Description>
         <Image src="./login.png" alt="login" />
         <ContainerButtons>
---
+++ pages/styled-components/ContainerButtons.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+export const ContainerButtons = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+`;
