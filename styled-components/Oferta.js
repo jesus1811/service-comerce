624070496cfe7deceef4a743d5e8b0e2c0a85619
jesus1811 <jayarza1811@gmail.com>
2022-06-07 18:10:21 -0500

    update oferta styled

--- styled-components/Oferta.js
+++ styled-components/Oferta.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 export const Oferta = styled.p`
   font-size: 20px;
   text-align: center;
-  color: #5f2120;
+  color: #014361;
   font-weight: 600;
   line-height: 23px;
-  background: #fff4e5;
+  background: #E5F6FD;
   padding: 8px;
   border-radius: 4px;
 `;
