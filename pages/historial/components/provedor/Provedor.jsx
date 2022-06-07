2fa423d723aaa7aecd43228f65997190a88dabba
jesus1811 <jayarza1811@gmail.com>
2022-06-07 16:59:58 -0500

    update prueba20

--- pages/historial/components/provedor/Provedor.jsx
+++ pages/historial/components/provedor/Provedor.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Card, Description, Subtitle } from "../../../../styled-components";
 import { Image } from "./Styled";
-
 const Provedor = ({ comprobanteDetalle }) => {
   return (
     <Card center>
